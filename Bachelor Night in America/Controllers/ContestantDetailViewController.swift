413eb9054bbb0d585e8cbd71a9b9d068e4f1aa8b
TJ Carney <tjcarney89@gmail.com>
2020-10-06 22:05:10 -0400

    Update Pick Button for Removing Pick

--- Bachelor Night in America/Controllers/ContestantDetailViewController.swift
+++ Bachelor Night in America/Controllers/ContestantDetailViewController.swift
@@ -73,8 +73,13 @@ class ContestantDetailViewController: UIViewController {
         } else if selectedContestant.status == .onShow && hasBeenPicked {
             self.statusLabel.backgroundColor = AppColors.green
             self.statusLabel.text = "On Show"
-            self.availabilityLabel.backgroundColor = AppColors.red
-            self.availabilityLabel.text = "Previously Picked"
+            if currentPick == selectedContestant.id {
+                self.availabilityLabel.backgroundColor = AppColors.green
+                self.availabilityLabel.text = "Current Pick"
+            } else {
+                self.availabilityLabel.backgroundColor = AppColors.red
+                self.availabilityLabel.text = "Previously Picked"
+            }
         } else if selectedContestant.status == .offShow {
             self.statusLabel.backgroundColor = AppColors.red
             self.statusLabel.text = "Eliminated"
@@ -99,8 +104,13 @@ class ContestantDetailViewController: UIViewController {
         }
         
         if Picks.store.allPicks.contains(selectedContestant.id) {
-            self.pickButton.isEnabled = false
-            self.pickButton.isHidden = true
+            if currentPick == selectedContestant.id {
+                self.pickButton.isEnabled = true
+                self.pickButton.isHidden = false
+            } else {
+                self.pickButton.isEnabled = false
+                self.pickButton.isHidden = true
+            }
         } else if selectedContestant.status == .offShow {
             self.pickButton.isEnabled = false
             self.pickButton.isHidden = true
@@ -114,12 +124,29 @@ class ContestantDetailViewController: UIViewController {
         if pickSubmitted {
             Picks.store.removePick(id: currentPick!)
             Picks.store.removeCurrentPick()
+            if currentPick != selectedContestant.id {
+                Picks.store.addPick(id: selectedContestant.id)
+                self.pickButton.isEnabled = false
+                self.pickButton.backgroundColor = .darkGray
+                self.pickButton.setTitle("Pick Submitted", for: .disabled)
+                self.availabilityLabel.backgroundColor = AppColors.green
+                self.availabilityLabel.text = "Current Pick"
+            } else {
+                self.pickButton.isEnabled = true
+                self.pickButton.backgroundColor = AppColors.red
+                self.pickButton.setTitle("Pick", for: .normal)
+                self.availabilityLabel.backgroundColor = AppColors.green
+                self.availabilityLabel.text = "Available"
+            }
+        } else {
+            Picks.store.addPick(id: selectedContestant.id)
+            self.pickButton.isEnabled = false
+            self.pickButton.backgroundColor = .darkGray
+            self.pickButton.setTitle("Pick Submitted", for: .disabled)
+            self.availabilityLabel.backgroundColor = AppColors.green
+            self.availabilityLabel.text = "Current Pick"
         }
-        Picks.store.addPick(id: selectedContestant.id)
         NotificationCenter.default.post(name: Notification.Name("didMakePick"), object: nil)
-        self.pickButton.isEnabled = false
-        self.pickButton.backgroundColor = .darkGray
-        self.pickButton.setTitle("Pick Submitted", for: .disabled)
     }
     
     /*
